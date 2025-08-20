3662750a0cc85517dcb58cb91278626de475d505
tv-avinash <tvavinash@gmail.com>
2025-08-20 18:23:59 +0530

    Add sitemap + robots.txt support

--- pages/api/sitemap.xml.js
+++ pages/api/sitemap.xml.js
@@ -1,11 +1,11 @@
 ﻿export default function handler(req, res) {
-  res.setHeader('Content-Type', 'application/xml');
-  res.status(200).send(<?xml version="1.0" encoding="UTF-8"?>
+  res.setHeader("Content-Type", "application/xml");
+  res.status(200).send(`<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://www.indianode.com/</loc>
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
   </url>
-</urlset>);
+</urlset>`);
 }
