45d8f4820d73129584497ba918c0148599a539b6
tv-avinash <tvavinash@gmail.com>
2025-08-20 16:17:34 +0530

    Add static sitemap.xml + robots.txt (and /api/sitemap.xml redirect)

--- pages/api/sitemap.xml.js
+++ pages/api/sitemap.xml.js
@@ -1,28 +1,4 @@
-// pages/api/sitemap.xml.js
-export default async function handler(req, res) {
-  const origin = process.env.NEXT_PUBLIC_SITE_ORIGIN || "https://www.indianode.com";
-
-  // Only include real pages you want indexed:
-  const urls = [
-    "/",                     // home
-    // "/whisper-gpu",
-    // "/stable-diffusion",
-    // "/llama",
-  ];
-
-  const lastmod = new Date().toISOString().split("T")[0];
-
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${urls.map(u => `<url>
-  <loc>${origin}${u}</loc>
-  <lastmod>${lastmod}</lastmod>
-  <changefreq>daily</changefreq>
-  <priority>${u === "/" ? "1.0" : "0.8"}</priority>
-</url>`).join("\n")}
-</urlset>`;
-
-  res.setHeader("Content-Type", "application/xml");
-  res.setHeader("Cache-Control", "public, s-maxage=3600, stale-while-revalidate=86400");
-  res.status(200).send(xml);
+export default function handler(req, res) {
+  res.setHeader("Location", "/sitemap.xml");
+  res.status(308).end();
 }
