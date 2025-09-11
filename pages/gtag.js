7f7e7e823a242c509b319cf530d3f3ddc49614ab
tv-avinash <tvavinash@gmail.com>
2025-09-11 09:04:33 +0530

    added gtag

---
+++ pages/gtag.js
@@ -0,0 +1,10 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-17546581584">
+</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-17546581584');
+</script>
